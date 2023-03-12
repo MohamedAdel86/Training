46008e281da31470c0a8a851cffab267fa8248aa
Mohamed Adel <113151084+MohamedAdel86@users.noreply.github.com>
2023-03-12 06:47:43 +0200

    Created the home page.

--- scripts/indexp.js
+++ scripts/indexp.js
@@ -1,1 +1,23 @@
+let myImage1 = document.querySelector('.im');
+num1=1;
+/*myImage1.onclick = () => {
+  let mySrc = myImage1.getAttribute('src');
+  num1=num1+1;
+  if(num1===11){num1=1} 
+  imgg= "images/cute-cat-"+num1+".jpg";
+  myImage1.setAttribute('src',imgg);
+}*/
+
+let myButton1 = document.querySelector('#b1');
+let myButton2 = document.querySelector('#b2');
+let myButton3 = document.querySelector('#b3');
+let myHeading = document.querySelector('h1');
+
+/*myButton3.onclick = () => {
+  }*/
+  myButton1.onclick = () => {
+  }
+  myButton2.onclick = () => {
+  }
+
 
